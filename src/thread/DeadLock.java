0f8001e968c2a6dd01528949b14d89ea0448eaa6
18645956947 <18645956947@163.com>
2019-09-12 16:20:44 +0800

    死锁简单的例子

---
+++ src/thread/DeadLock.java
@@ -0,0 +1,59 @@
+package thread;
+
+class A {
+    public synchronized void foo(B b){
+        System.out.println("当前线程名称："+Thread.currentThread().getName()+"进入了A的foo方法");
+
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("当前线程名称："+Thread.currentThread().getName()+"企图调用B实例的last()方法");
+        b.last();
+    }
+    public synchronized void last(){
+        System.out.println("进入了A类的last()方法内部");
+    }
+}
+class B{
+
+    public synchronized void bar(A a){
+        System.out.println("当前线程名称："+Thread.currentThread().getName()+"进入了B的bar方法");
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("当前线程名称："+Thread.currentThread().getName()+"企图调用A实例的last()方法");
+        a.last();
+    }
+
+    public synchronized void last(){
+        System.out.println("进入了B类的last()方法内部");
+    }
+}
+public class DeadLock implements Runnable{
+    A a= new A();
+    B b= new B();
+    public void init(){
+        Thread.currentThread().setName("主线程");
+        //调用a对象的foo()方法
+        a.foo(b);
+        System.out.println("进入了主线程后");
+    }
+    @Override
+    public void run() {
+        Thread.currentThread().setName("副线程");
+        //调用b对象的bar()方法
+        b.bar(a);
+        System.out.println("进入了副线程后");
+    }
+
+    public static void main(String[] args) {
+        DeadLock d1 = new DeadLock();
+        //以d1为target启动新线程
+        new Thread(d1).start();
+        d1.init();
+    }
+}
