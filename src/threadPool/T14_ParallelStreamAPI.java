5eb7d7492c105c6c86e0ffdfcaf20fac16f9b328
18645956947 <18645956947@163.com>
2019-10-08 16:33:16 +0800

    多线程实现 判断多个数是否是质数

---
+++ src/threadPool/T14_ParallelStreamAPI.java
@@ -0,0 +1,35 @@
+package threadPool;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class T14_ParallelStreamAPI {
+
+    public static void main(String[] args) {
+        List<Integer> nums = new ArrayList<>();
+        Random r = new Random();
+        for (int i = 0; i < 10000; i++) {
+            nums.add(100000 + r.nextInt(100000));
+        }
+        long start = System.currentTimeMillis();
+        nums.forEach(v -> isPrime(v));
+        long end = System.currentTimeMillis();
+        System.out.println("单线程花费时间是：" + (end - start));
+
+        start = System.currentTimeMillis();
+        nums.parallelStream().forEach(T14_ParallelStreamAPI::isPrime);
+        end = System.currentTimeMillis();
+        System.out.println("多线程花费时间是：" + (end - start));
+    }
+
+    public static boolean isPrime(int num){
+        for (int i = 2; i < Math.sqrt(num); i++) {
+            if(num % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
