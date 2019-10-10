12f63c0fe26c01b3e2acfd4fd9da710829b682dd
18645956947 <18645956947@163.com>
2019-10-10 21:29:47 +0800

    网络编程--get post 测试

--- src/network_programming/GetPostTest.java
+++ src/network_programming/GetPostTest.java
@@ -1,6 +1,8 @@
 package network_programming;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
@@ -32,12 +34,17 @@ public class GetPostTest {
             for (String key : map.keySet()) {
                 System.out.println(key + "--->" + map.get(key));
             }
-
-
-
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
+            try (BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream(), "utf-8"));)
+            {
+                String line;
+                while ((line = in.readLine()) != null){
+                    result += "\n"+line;
+                }
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+            return result;
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
