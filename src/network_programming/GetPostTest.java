7d70d39f14f2bb3ee90161089540dc8b0236b7d7
18645956947 <18645956947@163.com>
2019-10-12 19:12:15 +0800

    网络编程--get post 测试

--- src/network_programming/GetPostTest.java
+++ src/network_programming/GetPostTest.java
@@ -3,6 +3,7 @@ package network_programming;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
@@ -15,6 +16,13 @@ import java.util.Map;
  */
 public class GetPostTest {
 
+
+    /**
+     * 向制URL发送GET请求
+     * @param url 发送请求的URL
+     * @param param 请求参数 格式满足name1 = value1&name2 = value2的形式
+     * @return URL代表远程资源的响应
+     */
     public static String sendGet(String url, String param){
         String result = "";
         String urlName = url + "?" + param;
@@ -34,21 +42,78 @@ public class GetPostTest {
             for (String key : map.keySet()) {
                 System.out.println(key + "--->" + map.get(key));
             }
-            try (BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream(), "utf-8"));)
+
+            try (
+                    //定义输出流来读取URL的响应
+                    BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream(), "utf-8"));)
             {
                 String line;
                 while ((line = in.readLine()) != null){
                     result += "\n"+line;
                 }
-            } catch (IOException e1) {
-                e1.printStackTrace();
             }
             return result;
         } catch (Exception e) {
+            System.out.println("发送GET请求出现异常" + e);
             e.printStackTrace();
         }
 
-        return "aaa";
+        return result;
     }
 
+    /**
+     * 向制URL发送GET请求
+     * @param url 发送请求的URL
+     * @param param 请求参数 格式满足name1 = value1&name2 = value2的形式
+     * @return URL代表远程资源的响应
+     */
+    public static String sendPost(String url, String param){
+        String result = "";
+        try{
+            URL  realUrl = new URL(url);
+            //打开和URL之间的连接
+            URLConnection conn = realUrl.openConnection();
+            //设置通用的请求属性
+            conn.setRequestProperty("acccept", "*/*");
+            conn.setRequestProperty("connection", "Keep-Alive");
+            conn.setRequestProperty("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)");
+            //设置POST请求必须设置如下两行
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
+            try(
+                    //获取URLConnection 对象对应的输出流
+                    PrintWriter out = new PrintWriter(conn.getOutputStream()))
+            {
+                //发送请求参数
+                out.print(param);
+                //flush 输出流的缓冲
+                out.flush();
+            }
+            try(
+                //定义BufferReader 输入流读取url的响应
+                BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream(), "utf-8"))
+
+            )
+            {
+                String line;
+                while ((line = in.readLine()) != null){
+                    result += "\n" + line;
+                }
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        //发送get请求
+        String s = GetPostTest.sendGet("http://localhost:8888/abc/a.jsp", null);
+        System.out.println(s);
+
+        //发送post请求
+        String s1 = GetPostTest.sendPost("http://localhost:8888/abc/a.jsp", "name=crazyit.org&pass=leegang");
+        System.out.println(s1);
+    }
 }
