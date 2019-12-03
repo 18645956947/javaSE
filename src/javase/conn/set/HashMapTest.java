47f7cdeed5ab6b1ce25be0a37a269101047db6c6
18645956947 <18645956947@163.com>
2019-12-03 20:26:17 +0800

    测试

--- src/javase/conn/set/HashMapTest.java
+++ src/javase/conn/set/HashMapTest.java
@@ -1,6 +1,9 @@
 package javase.conn.set;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 
 public class HashMapTest {
 
@@ -17,5 +20,40 @@ public class HashMapTest {
         for (Integer key : map.keySet()) {
             System.out.print(key + " -> ");
         }
+        System.out.println();
+        Integer aa = 127;
+        Integer b = 127;
+
+        System.out.println(aa == b);
+        System.out.println(aa.equals(b));
+
+        aa = 128;
+        b = 128;
+        System.out.println(aa == b);
+        System.out.println(aa.equals(b));
+        Integer i1 = new Integer(2);
+        Integer i2 = new Integer(2);
+        System.out.println(i1 == i2);
+        System.out.println(i1.equals(i2));
+
+        System.out.println("--------");
+
+        List<String> l1 = new ArrayList<>();
+        l1.add("1");
+        l1.add("2");
+        l1.add("3");
+        l1.add("4");
+        l1.add("5");
+        l1.add("6");
+        Iterator<String> iterator = l1.iterator();
+        while (iterator.hasNext()){
+            String item = iterator.next();
+            if(item.equals("5")){
+                iterator.remove();
+            }
+        }
+        System.out.print(l1);
     }
+
+
 }
